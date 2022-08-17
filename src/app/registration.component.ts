592161c421b50232538e9b21c3d25742439a0926
LiKoloz <Ptvkzr_YbrekbyfH@mail.ru>
2022-08-17 17:21:24 +0300

    stableversion+

--- src/app/registration.component.ts
+++ src/app/registration.component.ts
@@ -3,10 +3,26 @@ import {Component} from "@angular/core";
 @Component({
   selector: 'reg-comp',
   template: `
-    <main>
-  <div class="">Регистрация</div>
-      </main>
-  `
+    <main class="w-full h-full ng-blue-300 ">
+      <div class="flex flex-row flex-nowrap justify-center place-items-center">
+        <div class="basis-1/4 "></div>
+        <div class="basis-1/2 text-center">
+          <h2 class="text-5xl zagolovok">Регистрация</h2>
+          <div>33</div>
+        </div>
+        <div class="basis-1/4 "></div>
+      </div>
+    </main>
+  `,
+  styles: [`
+  .zagolovok{
+    padding: 15%;
+    color: white;
+    font-family: Roboto, Arial, sans-serif;
+
+    text-align: center;
+  }
+  `]
 })
 
 export class RegistrationComponent {
